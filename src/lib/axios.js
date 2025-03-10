797b09b521651ff27238d2af99c11d311499e4f7
Rupayan <154427141+Rupayan0001@users.noreply.github.com>
2025-03-11 05:01:31 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://renokon-backend.onrender.com/api/v1",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
 
-// https://renokon-backend.onrender.com/api/v1
+// https://renokon-backend.onrender.com/api/v1   http://localhost:5000/api/v1
