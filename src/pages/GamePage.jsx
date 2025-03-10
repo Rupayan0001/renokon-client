0e886c0a7d6094c72d615cb2ad829cc6076b9e13
Rupayan <154427141+Rupayan0001@users.noreply.github.com>
2025-03-11 04:41:22 +0530

    Update GamePage.jsx

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -18,7 +18,7 @@ import TopButtonsNav from "../components/layout/game/TopButtonsNav.jsx";
 import PoolCard from "../components/layout/game/PoolCard.jsx";
 // import { PoolDataLists } from "../lib/poolDataLists.js";
 import MiddleTitle from "../components/layout/game/MiddleTitle.jsx";
-import Loader from "../components/layout/loader.jsx";
+import Loader from "../components/layout/Loader.jsx";
 import Notification from "../components/layout/Notification.jsx";
 import GameFilterButtons from "../components/layout/game/GameFilterButtons.jsx";
 import { groupGamePools } from "../lib/groupGamePools.js";
