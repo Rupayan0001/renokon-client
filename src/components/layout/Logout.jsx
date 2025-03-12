7a56d4a54d363b9f5c994b5db7a176114f31ef38
Rupayan <154427141+Rupayan0001@users.noreply.github.com>
2025-03-12 16:16:15 +0530

    Update Logout.jsx

--- src/components/layout/Logout.jsx
+++ src/components/layout/Logout.jsx
@@ -4,7 +4,7 @@ const Logout = () => {
   return (
     <dialog className="fixed inset-0 z-50 h-[100%] w-[100%]  flex justify-center items-center bg-black bg-opacity-80">
       <div className="h-[150px] w-[300px] rounded-xl flex flex-col justify-center items-center text-[20px]">
-        <p className="spinOnButton h-[30px] w-[30px]"></p>
+        <p className="spinButton h-[30px] w-[30px]"></p>
       </div>
     </dialog>
   );
