5cf9e6c10ad9da4bdb2b7c03d4137bb1c25898cf
Rupayan <154427141+Rupayan0001@users.noreply.github.com>
2025-03-12 20:18:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  assetsDir: "assets", 
   optimizeDeps: {
     exclude: ['process'],  // Exclude dotenv from frontend bundle
   },
