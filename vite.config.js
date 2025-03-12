2f6b278252c4087b3f6671feb07799cb4bb3e912
Rupayan <154427141+Rupayan0001@users.noreply.github.com>
2025-03-13 04:30:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react(), viteCompression({ algorithm: 'brotliCompress' })],
+  plugins: [react(), viteCompression({ algorithm: 'brotliCompress' }), viteCompression({ algorithm: 'gzip' })],
   base: "/",
   build: {
     rollupOptions: {
