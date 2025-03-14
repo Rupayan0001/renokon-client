fe11a6107d436178f632ef042e22440f25889609
Rupayan <rupayan.team@gmail.com>
2025-03-15 03:46:40 +0530

    Changed hero images

--- src/components/layout/game/HeroSlider.jsx
+++ src/components/layout/game/HeroSlider.jsx
@@ -8,45 +8,27 @@ import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { Link } from "react-router-dom";
 
-import cricketImg from "../../../assets/images/ms-dhoni-team-india-national-cricket-team-shikhar-dhawan-wallpaper-preview.jpg";
 import bollywoodImg2 from "../../../assets/images/Pathaan-1920x1080-2.jpg";
-import footbalImg from "../../../assets/images/360_F_589743912_Hb0wJQKoUZIXXUKkejDWJUUwBopHha92.jpg";
-import bollywoodImg from "../../../assets/images/srk-jawan-1200.jpg";
+import footballImg2 from "../../../assets/images/f5LSNK.webp";
 import musicImg from "../../../assets/images/Arijit-Singh-.jpg";
-import financeImg from "../../../assets/images/1085f340-bfeb-424e-a9d5-723d1f63d29b (1).jpg";
-import personalityImg from "../../../assets/images/PM-Narendra-Modi-iPhone-Wallpapers.jpg";
+import financeImg from "../../../assets/images/529_Stock_Market_1920x1080_a7d91d2e997b49409920780a817a7a47.jpg";
+import personalityImg from "../../../assets/images/personalities.jpg";
 import cric2 from "../../../assets/images/1315984.jpg";
-import geographyImg from "../../../assets/images/indian-flag-abstract-background-wallpaper-for-independence-day-on-15-august-ai-generative-free-photo.jpg";
-import historyImg from "../../../assets/images/Freedom_fighters.webp";
-// import historyImg from "../../../assets/images/reimagining-netaji-subhash-chandra-bose-through-ai-v0-0rjcpva5i4ec1d.jpg";
-import mathImg from "../../../assets/images/pngtree-3d-rendering-of-calculator-symbol-against-a-black-background-image_3782739d.jpg";
-import footballImg2 from "../../../assets/images/cristiano-ronaldo-cr7-the-kick-facebook-cover.jpg";
-import businessImg from "../../../assets/images/pngtree-partnership-of-companies-collaboration-business-technology-internet-concept-image_15659993.jpg";
-
+import geographyImg from "../../../assets/images/R (2).jpg";
+import historyImg from "../../../assets/images/OIP (2).jpg";
+import mathImg from "../../../assets/images/360_F_540586727_w1zF3FaMFlvmfO1BgIcN2SVmnKrsl10G.jpg";
+import businessImg from "../../../assets/images/rich.jpg";
 const slides = [
-  { id: 1, image: cric2, title: "Mega Quiz Pool - Win $5000", link: "/game/cricket" },
-  { id: 2, image: footbalImg, title: "Daily Cash Quiz - $1000 Prize", link: "/game/football" },
-  { id: 3, image: bollywoodImg, title: "Daily Cash Quiz - $1000 Prize", link: "/game/bollywood" },
-  { id: 4, image: musicImg, title: "Esports Quiz Battle - $3000", link: "/game/music" },
-  { id: 5, image: businessImg, title: "Esports Quiz Battle - $3000", link: "/game/business" },
-  { id: 6, image: financeImg, title: "Esports Quiz Battle - $3000", link: "/game/finance" },
-  { id: 7, image: personalityImg, title: "Esports Quiz Battle - $3000", link: "/game/personality" },
-  { id: 8, image: geographyImg, title: "Esports Quiz Battle - $3000", link: "/game/geography" },
-  { id: 9, image: historyImg, title: "Crypto Trivia Challenge - $2000", link: "/game/history" },
-  { id: 10, image: mathImg, title: "Daily Cash Quiz - $1000 Prize", link: "/game/maths" },
-];
-const slides2 = [
-  // { id: 1, image: bollywoodImg2, title: "Mega Quiz Pool - Win $5000", link: "/game/bollywood" },
-  { id: 1, image: cric2, title: "Mega Quiz Pool - Win $5000", link: "/game/cricket" },
-  { id: 2, image: footballImg2, title: "Daily Cash Quiz - $1000 Prize", link: "/game/football" },
-  { id: 3, image: bollywoodImg2, title: "Daily Cash Quiz - $1000 Prize", link: "/game/bollywood" },
-  { id: 4, image: musicImg, title: "Esports Quiz Battle - $3000", link: "/game/music" },
-  { id: 5, image: businessImg, title: "Esports Quiz Battle - $3000", link: "/game/business" },
-  { id: 6, image: financeImg, title: "Esports Quiz Battle - $3000", link: "/game/finance" },
-  { id: 7, image: personalityImg, title: "Esports Quiz Battle - $3000", link: "/game/personality" },
-  { id: 8, image: geographyImg, title: "Esports Quiz Battle - $3000", link: "/game/geography" },
-  { id: 9, image: historyImg, title: "Crypto Trivia Challenge - $2000", link: "/game/history" },
-  { id: 10, image: mathImg, title: "Daily Cash Quiz - $1000 Prize", link: "/game/maths" },
+  { id: 1, image: cric2, title: "Cricket", link: "/game/cricket" },
+  { id: 2, image: footballImg2, title: "Football", link: "/game/football" },
+  { id: 3, image: bollywoodImg2, title: "Bollywood", link: "/game/bollywood" },
+  { id: 4, image: musicImg, title: "Music", link: "/game/music" },
+  { id: 5, image: businessImg, title: "Businessman", link: "/game/business" },
+  { id: 6, image: financeImg, title: "Finance", link: "/game/finance" },
+  { id: 7, image: personalityImg, title: "Famous Personalities", link: "/game/personality" },
+  { id: 8, image: geographyImg, title: "Geography", link: "/game/geography" },
+  { id: 9, image: historyImg, title: "History", link: "/game/history" },
+  { id: 10, image: mathImg, title: "Maths", link: "/game/maths" },
 ];
 
 const HeroSlider = ({ width }) => {
@@ -71,7 +53,11 @@ const HeroSlider = ({ width }) => {
   });
 
   return (
-    <div className={`relative w-[95%] max-w-[768px] ${width > 768 ? "h-[250px]" : "h-[130px]"} bg-black mt-3 mx-auto overflow-hidden rounded-xl shadow-lg`}>
+    <div
+      className={`relative w-[95%] max-w-[768px] ${width >= 768 && "h-[250px]"} ${width < 768 && width > 450 && "h-[175px]"} ${
+        width <= 450 && "h-[130px]"
+      } bg-black mt-3 mx-auto overflow-hidden rounded-xl shadow-lg`}
+    >
       {width > 525 ? (
         <Swiper
           modules={[Navigation, Pagination, Autoplay]}
@@ -81,20 +67,23 @@ const HeroSlider = ({ width }) => {
           autoplay={{ delay: 4000, disableOnInteraction: false }}
           className="w-full"
         >
-          {slides2.map((slide) => (
+          {slides.map((slide) => (
             <SwiperSlide key={slide.id}>
               <AnimatePresence>
                 <Link to={slide.link}>
-                  <motion.img
-                    key={slide.id}
-                    src={slide.image}
-                    alt={`Slide ${slide.id}`}
-                    className="w-full h-full object-cover"
-                    initial={{ opacity: 0, y: 0 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: 0 }}
-                    transition={{ duration: 1, ease: "easeInOut" }}
-                  />
+                  <div className="w-full h-full relative">
+                    {/* <p className="absolute top-0 left-0 text-white bg-black text-md px-2 py-0 rounded-xl font-bold z-50">{slide.title}</p> */}
+                    <motion.img
+                      key={slide.id}
+                      src={slide.image}
+                      alt={`Slide ${slide.id}`}
+                      className="w-full h-full object-cover"
+                      initial={{ opacity: 0, y: 0 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0, y: 0 }}
+                      transition={{ duration: 1, ease: "easeInOut" }}
+                    />
+                  </div>
                 </Link>
               </AnimatePresence>
             </SwiperSlide>
