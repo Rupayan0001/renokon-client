549066ac198234ccb428e8d6add89efaec022085
Rupayan <rupayan.team@gmail.com>
2025-03-15 18:17:25 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const EnviroMent = import.meta.env.VITE_ENVIRONMENT;
 
 export const axiosInstance = axios.create({
-  baseURL: EnviroMent === "development" ? "http://localhost:5000/api/v1" : "https://renokon-backend.onrender.com/api/v1",
+  baseURL: EnviroMent === "development" ? "http://localhost:5000/api/v1" : "https://renokon-backend-production.up.railway.app/api/v1/",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
